81b3ba0e1ec890eefe5782f74e6a68c3f3b62451
Momo Jon <nemo17@nate.com>
2025-04-09 23:48:45 +0900

    프로젝트 초기 설정 및 기본 파일 추가

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Projects.css';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <p>Here you can showcase your GitHub projects.</p>
+    </div>
+  );
+}
+
+export default Projects;
